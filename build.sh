e221245488fd8d96523e55a66b03add683442291
Narek-D8v <haroyannarek090@gmail.com>
2026-07-02 21:23:45 +0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
-# Скачиваем статический ffmpeg для Linux
-curl -L https://github.com/eugeneware/ffmpeg-static/releases/download/b5.0.1/ffmpeg-linux-x64 -o ffmpeg
 chmod +x ffmpeg
-mv ffmpeg /usr/local/bin/
+pip install -r requirements.txt
