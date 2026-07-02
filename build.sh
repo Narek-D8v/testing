ce2c4c299ac2371a5697cf4fecd25ba18c6bbe0e
Narek-D8v <haroyannarek090@gmail.com>
2026-07-02 21:19:16 +0400

    Update build script to download static ffmpeg
    Replaced installation of ffmpeg with download of static binary.

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-apt-get update
-apt-get install -y ffmpeg
+
+# Скачиваем статический ffmpeg для Linux
+curl -L https://github.com/eugeneware/ffmpeg-static/releases/download/b5.0.1/ffmpeg-linux-x64 -o ffmpeg
+chmod +x ffmpeg
+mv ffmpeg /usr/local/bin/
